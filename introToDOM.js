182c2ac2be9a0f3a5b4b7f4bca8000e8f4445f20
ApolloSolo <matt1teixeira@gmail.com>
2021-11-22 18:21:35 -0500

    Commit

---
+++ introToDOM.js
@@ -0,0 +1,22 @@
+//Document Object Models - a JavaScript representation of a webpage
+//The Document Object - when a page loads, for each HTML and CSS, a JS object is created.
+/* <body>
+        <h1>
+            <ul>
+                <li>Water Plants</li>
+                <li>Get Some Sleep</li>
+            </ul>
+        </h1>
+    </body> 
+*/
+// HTML + CSS go in... JS Objects come out
+ /*                                 DOCUMENT
+                                        \
+                                        BODY
+                                        /   \
+                                       /     \
+                                      H1     UR
+                                            /\
+                                           /  \
+                                          LI  LI
+*/
